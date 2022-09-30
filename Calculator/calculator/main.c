0cb89ccfe7dd6bdb742ba235c3838b16583cdd33
mazaribahaduri <95102369+mazaribahaduri@users.noreply.github.com>
2022-09-30 17:29:32 +0100

    Advanced C calculator
    This commit implements all features of an advanced calculator.

--- Calculator/calculator/main.c
+++ Calculator/calculator/main.c
@@ -5,10 +5,30 @@ int main()
 {
     double number1;
     double number2;
-    printf("Enter first number: ");
+    char operation;
+
+    printf("Enter a number: ");
     scanf("%lf", &number1);
-    printf("Enter second number: ");
+    printf("Enter operation mode: ");
+    scanf(" %c", &operation);
+    printf("Enter your second number: ");
     scanf("%lf", &number2);
-    printf("Answer: %f", number1 + number2);
+
+    //Now we need the algorithm.
+    if (operation == '+'){
+        printf("%lf", number1 + number2);
+    }
+    else if (operation == '-'){
+        printf("%lf",number1 - number2);
+    }
+    else if (operation == '*'){
+        printf("%lf",number1 * number2);
+    }
+    else if (operation == '/'){
+        printf("%lf",number1 / number2);
+    }
+    else{
+        printf("Invalid operation.");
+    }
     return 0;
 }
